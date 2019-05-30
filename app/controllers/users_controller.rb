3c188c4c0a1f26be6ac800bcaae70fa5f48192f2
Your Name <giomay321@gmail.com>
2019-05-30 22:25:18 +0700

    Chapter 11 : Account activation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,16 +13,18 @@ class UsersController < ApplicationController
       per_page: Settings.per_page
   end
 
-  def show; end
+  def show
+    redirect_to root_url && return unless @user.activated
+  end
 
   def edit; end
 
   def create
     @user = User.new user_params
     if @user.save
-      log_in @user
-      flash.now[:success] = t "label.welcome"
-      redirect_to @user
+      @user.send_activation_email
+      flash[:info] = t "text.pls_check_mail"
+      redirect_to root_url
     else
       render :new
     end
