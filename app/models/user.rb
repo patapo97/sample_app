6e626137caad662b333ab448a54c51a69bcade91
Your Name <giomay321@gmail.com>
2019-05-30 22:25:18 +0700

    Chapter 7: Sign up

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,11 +3,13 @@ class User < ApplicationRecord
 
   has_secure_password
 
-  validates :name, presence: true, length: {maximum: Settings.name_length}
-  validates :email, presence: true, length: {maximum: Settings.email_length},
+  validates :name, presence: true,
+    length: {maximum: Settings.maximum_name_length}
+  validates :email, presence: true,
+    length: {maximum: Settings.maximum_email_length},
     format: {with: VALID_EMAIL_REGEX}, uniqueness: {case_sensitive: false}
   validates :password, presence: true,
-    length: {minimum: Settings.password_min_length}
+    length: {minimum: Settings.minimum_password_length}
 
   before_save ->{email.downcase!}
 end
